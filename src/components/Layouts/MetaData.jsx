85ba22079533036273cad1c7bfaf224020de8722
SandeepaNagaraj <sandeepanagaraj4@gmail.com>
2023-11-30 14:28:56 +0530

    sample

---
+++ src/components/Layouts/MetaData.jsx
@@ -0,0 +1,11 @@
+import { Helmet } from "react-helmet-async";
+
+const MetaData = ({ title }) => {
+    return (
+        <Helmet>
+            <title>Circle of FOOD</title>
+        </Helmet>
+    );
+};
+
+export default MetaData;
---
+++ src/utils/constants.js
@@ -0,0 +1,59 @@
+export const BASE_PROFILE_IMAGE_URL='http://localhost:4000/public/uploads/profiles/';
+export const BASE_POST_IMAGE_URL='http://localhost:4000/public/uploads/posts/';
+
+export const SOCKET_ENDPOINT = "http://localhost:4000";
+
+export const stories = [
+    {
+        title: "Dessert",
+        image: "javascript"
+    },
+    {
+        title: "Meals",
+        image: "nodejs"
+    },
+    {
+        title: "Breakfast",
+        image: "expressjs"
+    },
+    {
+        title: "Idili",
+        image: "mongodb"
+    },
+    {
+        title: "Grill Chicken",
+        image: "reactjs"
+    },
+    {
+        title: "Full Meals",
+        image: "socketio"
+    },
+    {
+        title: "Rice Bowl",
+        image: "tailwind"
+    },
+    {
+        title: "Sushi",
+        image: "mui"
+    },
+    {
+        title: "Butter Chicken",
+        image: "redux"
+    },
+    {
+        title: "Chettinad Gravy",
+        image: "multer"
+    },
+    {
+        title: "Hyderbad Briyani",
+        image: "sendgrid"
+    },
+    {
+        title: "Axios",
+        image: "axios"
+    },
+    {
+        title: "Fish",
+        image: "toastify"
+    },
+]
